6ee1503d12b3538873f950f4ebba2fb15b64374c
Deepthy Nair <dpthynair@Deepthys-MacBook-Pro.local>
2020-03-04 11:32:43 +0530

    Updates

---
+++ precache-manifest.fb79fc4ff104aafe3e926e449bc8380c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "440fc1d937c8245e30ac254c10ba8fa1",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "70401bada8e374fef6e4",
+    "url": "/monsters-rolodex/static/css/main.2618736c.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70401bada8e374fef6e4",
+    "url": "/monsters-rolodex/static/js/main.ac6a170a.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
